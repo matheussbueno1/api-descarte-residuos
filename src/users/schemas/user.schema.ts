c098a390545411424088388cc40784232a4e2784
matheussbueno1 <matheussilvabueno24@gmail.com>
2025-11-16 00:33:54 -0300

    Add files via upload

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,16 @@
+// src/users/schemas/user.schema.ts
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: false })
+  email?: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
